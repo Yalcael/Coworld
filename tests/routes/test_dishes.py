5383b1bafcd58dabc7916f682ba21b5121f174e4
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-05-16 19:03:43 +0200

    Coworld in progress

---
+++ tests/routes/test_dishes.py
@@ -0,0 +1,48 @@
+import uuid
+from datetime import datetime
+from unittest.mock import AsyncMock
+import pytest
+from fastapi import FastAPI
+from starlette.testclient import TestClient
+
+from coworld.controllers.dishes import DishController
+from coworld.dependencies import get_dish_controller
+from coworld.models.dishes import Category
+from coworld.models.models import Dish
+
+
+@pytest.mark.asyncio
+async def test_get_dish_by_id(
+    dish_controller: DishController, app: FastAPI, client: TestClient
+):
+    _id = uuid.uuid4()
+
+    def _mock_get_dish_by_id():
+        dish_controller.get_dish_by_id = AsyncMock(
+            return_value=Dish(
+                id=_id,
+                created_at=datetime(2020, 1, 1),
+                category=Category("PLATS"),
+                title="Amazing Cow",
+                description="The Amazing Cow burger, juicy and tasty.",
+                ingredients="Meat, Salad, Tomato, Cheese",
+                price=6.99,
+                halal=False,
+            ),
+        )
+        return dish_controller
+
+    app.dependency_overrides[get_dish_controller] = _mock_get_dish_by_id
+
+    get_dish_by_id_response = client.get(f"/dishes/{_id}")
+    assert get_dish_by_id_response.status_code == 200
+    assert get_dish_by_id_response.json() == {
+        "id": str(_id),
+        "created_at": "2020-01-01T00:00:00",
+        "category": "PLATS",
+        "title": "Amazing Cow",
+        "description": "The Amazing Cow burger, juicy and tasty.",
+        "ingredients": "Meat, Salad, Tomato, Cheese",
+        "price": 6.99,
+        "halal": False,
+    }
