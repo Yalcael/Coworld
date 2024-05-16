5d3eb6af7ef6ef93f20ad84df1103e4833efa592
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-05-16 21:24:47 +0200

    Coworld in progress

--- tests/routes/test_dishes.py
+++ tests/routes/test_dishes.py
@@ -8,6 +8,7 @@ from starlette.testclient import TestClient
 from coworld.controllers.dishes import DishController
 from coworld.dependencies import get_dish_controller
 from coworld.models.dishes import Category
+from coworld.models.errors import DishNotFoundError
 from coworld.models.models import Dish
 
 
@@ -45,4 +46,27 @@ async def test_get_dish_by_id(
         "ingredients": "Meat, Salad, Tomato, Cheese",
         "price": 6.99,
         "halal": False,
+    }
+
+
+@pytest.mark.asyncio
+async def test_get_dish_by_id_raise_no_result_found_error(
+    dish_controller: DishController, app: FastAPI, client: TestClient
+):
+    _id = uuid.uuid4()
+
+    def _mock_get_dish_by_id():
+        dish_controller.get_dish_by_id = AsyncMock(
+            side_effect=DishNotFoundError(dish_id=_id)
+        )
+        return dish_controller
+
+    app.dependency_overrides[get_dish_controller] = _mock_get_dish_by_id
+
+    get_dish_by_id_response = client.get(f"/dishes/{_id}")
+    assert get_dish_by_id_response.status_code == 404
+    assert get_dish_by_id_response.json() == {
+        "message": f"Dish with ID {_id} not found",
+        "name": "DishNotFoundError",
+        "status_code": 404,
     }
