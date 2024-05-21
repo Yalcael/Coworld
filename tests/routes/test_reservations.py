1e1fdd44372ee968f6635f68ce15046acba73da4
Vincent <141185796+Yalcael@users.noreply.github.com>
2024-05-21 09:08:58 +0200

    Coworld in progress

--- tests/routes/test_reservations.py
+++ tests/routes/test_reservations.py
@@ -15,7 +15,7 @@ from coworld.models.reservations import Reservation, ReservationCategory
 
 @pytest.mark.asyncio
 async def test_get_reservation_by_id(
-    reservation_controller: ReservationController, app: FastAPI, client: TestClient
+        reservation_controller: ReservationController, app: FastAPI, client: TestClient
 ):
     _id = uuid.uuid4()
 
@@ -54,7 +54,7 @@ async def test_get_reservation_by_id(
 
 @pytest.mark.asyncio
 async def test_get_reservation_by_id_raise_no_result_found_error(
-    reservation_controller: ReservationController, app: FastAPI, client: TestClient
+        reservation_controller: ReservationController, app: FastAPI, client: TestClient
 ):
     _id = uuid.uuid4()
 
@@ -77,7 +77,7 @@ async def test_get_reservation_by_id_raise_no_result_found_error(
 
 @pytest.mark.asyncio
 async def test_get_reservations(
-    reservation_controller: ReservationController, app: FastAPI, client: TestClient
+        reservation_controller: ReservationController, app: FastAPI, client: TestClient
 ):
     mock_reservations = [
         Reservation(
@@ -126,3 +126,149 @@ async def test_get_reservations(
         }
         for reservation in mock_reservations
     ]
+
+
+@pytest.mark.asyncio
+async def test_create_reservation(
+        reservation_controller: ReservationController, app: FastAPI, client: TestClient
+):
+    reservation_data = {
+        "reservation_category": "SIMPLE",
+        "name": "aaaaaa",
+        "family_name": "vvvvvv",
+        "amount_of_people": 6,
+        "email_address": "vvvvv@admin.com",
+        "reservation_time": "2020-03-03T20:30:00",
+        "phone_number": "+33633445566"
+    }
+
+    mock_reservation = Reservation(
+        id=uuid.uuid4(),
+        created_at=datetime(2020, 1, 1),
+        reservation_category=ReservationCategory(reservation_data["reservation_category"]),
+        reservation_time=datetime.fromisoformat(reservation_data["reservation_time"]),
+        **{k: v for k, v in reservation_data.items() if k not in ["reservation_category", "reservation_time"]},
+    )
+
+    def _mock_create_reservation():
+        reservation_controller.create_reservation = AsyncMock(return_value=mock_reservation)
+        return reservation_controller
+
+    app.dependency_overrides[get_reservation_controller] = _mock_create_reservation
+
+    create_reservation_response = client.post("/reservations", json=reservation_data)
+    assert create_reservation_response.status_code == 201
+    assert create_reservation_response.json() == {
+        "id": str(mock_reservation.id),
+        "created_at": mock_reservation.created_at.isoformat(),
+        "reservation_category": mock_reservation.reservation_category.name,
+        "name": mock_reservation.name,
+        "family_name": mock_reservation.family_name,
+        "amount_of_people": mock_reservation.amount_of_people,
+        "email_address": mock_reservation.email_address,
+        "reservation_time": mock_reservation.reservation_time.isoformat(),
+        "phone_number": str(mock_reservation.phone_number)
+    }
+
+
+@pytest.mark.asyncio
+async def test_update_reservation(
+    reservation_controller: ReservationController, app: FastAPI, client: TestClient
+):
+    _id = uuid.uuid4()
+    reservation_update_data = {
+        "reservation_category": "SIMPLE",
+        "name": "aaaaaa",
+        "family_name": "vvvvvv",
+        "amount_of_people": 6,
+        "email_address": "vvvvv@admin.com",
+        "reservation_time": "2020-03-03T20:30:00",
+        "phone_number": "+33633445566"
+    }
+
+    updated_reservation = Reservation(
+        id=_id,
+        created_at=datetime(2020, 1, 1),
+        reservation_category=ReservationCategory(reservation_update_data["reservation_category"]),
+        reservation_time=datetime.fromisoformat(reservation_update_data["reservation_time"]),
+        **{k: v for k, v in reservation_update_data.items() if k not in ["reservation_category", "reservation_time"]},
+    )
+
+    def _mock_update_reservation():
+        reservation_controller.update_reservation = AsyncMock(return_value=updated_reservation)
+        return reservation_controller
+
+    app.dependency_overrides[get_reservation_controller] = _mock_update_reservation
+
+    update_reservation_response = client.patch(f"/reservations/{_id}", json=reservation_update_data)
+    assert update_reservation_response.status_code == 200
+    assert update_reservation_response.json() == {
+        "id": str(_id),
+        "created_at": updated_reservation.created_at.isoformat(),
+        "reservation_category": updated_reservation.reservation_category.name,
+        "name": updated_reservation.name,
+        "family_name": updated_reservation.family_name,
+        "amount_of_people": updated_reservation.amount_of_people,
+        "email_address": updated_reservation.email_address,
+        "reservation_time": updated_reservation.reservation_time.isoformat(),
+        "phone_number": str(updated_reservation.phone_number)
+    }
+
+
+@pytest.mark.asyncio
+async def test_update_reservation_not_found_error(
+    reservation_controller: ReservationController, client: TestClient, app: FastAPI
+):
+    _id = uuid.uuid4()
+
+    reservation_update_data = {
+        "reservation_category": "SIMPLE",
+        "name": "UpdatedAaaaaa",
+        "family_name": "vvvvvv",
+        "amount_of_people": 6,
+        "email_address": "vvvvv@admin.com",
+        "reservation_time": "2020-03-03T20:30:00",
+        "phone_number": "+33633445566"
+    }
+
+    def _mock_update_reservation():
+        reservation_controller.update_reservation = AsyncMock(
+            side_effect=ReservationNotFoundError(reservation_id=_id)
+        )
+        return reservation_controller
+
+    app.dependency_overrides[get_reservation_controller] = _mock_update_reservation
+    update_reservation_response = client.patch(f"/reservations/{_id}", json=reservation_update_data)
+    assert update_reservation_response.status_code == 404
+
+
+@pytest.mark.asyncio
+async def test_delete_reservation(
+    reservation_controller: ReservationController, app: FastAPI, client: TestClient
+):
+    _id = uuid.uuid4()
+
+    def _mock_delete_reservation():
+        reservation_controller.delete_reservation = AsyncMock(return_value=None)
+        return reservation_controller
+
+    app.dependency_overrides[get_reservation_controller] = _mock_delete_reservation
+    delete_reservation_response = client.delete(f"/reservations/{_id}")
+    assert delete_reservation_response.status_code == 204
+
+
+@pytest.mark.asyncio
+async def test_delete_reservation_not_found_error(
+    app: FastAPI, client: TestClient, reservation_controller: ReservationController
+):
+    _id = uuid.uuid4()
+
+    def _mock_delete_reservation():
+        reservation_controller.delete_reservation = AsyncMock(
+            side_effect=ReservationNotFoundError(reservation_id=_id)
+        )
+        return reservation_controller
+
+    app.dependency_overrides[get_reservation_controller] = _mock_delete_reservation
+    delete_reservation_response = client.delete(f"/reservations/{_id}")
+    assert delete_reservation_response.status_code == 404
